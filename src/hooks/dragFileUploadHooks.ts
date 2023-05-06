893a6fad49ecc980c42212c9d511d1eb0b97e7ef
cain.chu <cainmaila>
2023-05-06 20:22:01 +0800

    檔案拖曳

---
+++ src/hooks/dragFileUploadHooks.ts
@@ -0,0 +1,47 @@
+import { useLayoutEffect, useState } from 'react'
+
+const reader = new FileReader()
+export const useDragFileUpload = (dropbox: HTMLElement | null) => {
+  const [data, setData] = useState<string | ArrayBuffer | null>(null)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  useLayoutEffect(() => {
+    dropbox?.addEventListener('dragenter', dragenter, false)
+    dropbox?.addEventListener('dragover', dragover, false)
+    dropbox?.addEventListener('drop', drop, false)
+
+    function dragenter(e: DragEvent) {
+      e.stopPropagation() //終止事件傳導
+      e.preventDefault() //終止預設行為
+    }
+
+    function dragover(e: DragEvent) {
+      e.stopPropagation() //終止事件傳導
+      e.preventDefault() //終止預設行為
+    }
+
+    function drop(e: DragEvent) {
+      e.stopPropagation() //終止事件傳導
+      e.preventDefault() //終止預設行為
+      e.dataTransfer && handleFiles(e.dataTransfer.files)
+    }
+
+    function handleFiles(files: FileList) {
+      setData(null)
+      setIsLoading(true)
+      reader.onload = (e) => {
+        setData(e.target?.result || null)
+        setIsLoading(false)
+      }
+      reader.readAsDataURL(files[0])
+    }
+
+    return () => {
+      dropbox?.removeEventListener('dragenter', dragenter, false)
+      dropbox?.removeEventListener('dragover', dragover, false)
+      dropbox?.removeEventListener('drop', drop, false)
+    }
+  }, [dropbox])
+
+  return { data, isLoading }
+}
--- src/view/Viewer.tsx
+++ src/view/Viewer.tsx
@@ -1,9 +1,15 @@
 import { useThreeSceneInit } from '../hooks/threeSceneHooks'
-
+import { useDragFileUpload } from '../hooks/dragFileUploadHooks'
+import { useEffect } from 'react'
 function Viewer() {
   const { viewerRef } = useThreeSceneInit()
+  const { data } = useDragFileUpload(viewerRef.current)
 
-  return <div className="Viewer" ref={viewerRef}></div>
+  useEffect(() => {
+    console.log(data)
+  }, [data])
+
+  return <div id="Viewer" ref={viewerRef}></div>
 }
 
 export default Viewer
