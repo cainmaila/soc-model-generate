73ea0b91be9dfc743bf02039d7ee3d369446dc3a
cain.chu <cainmaila>
2023-05-07 18:34:03 +0800

    下載

--- src/view/Viewer.tsx
+++ src/view/Viewer.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { useThreeSceneInit } from '../hooks/threeSceneHooks'
 import { useDragFileUpload } from '../hooks/dragFileUploadHooks'
-import { useEffect, useState } from 'react'
-import { LoadingManager } from 'three'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { useCallback, useEffect, useState } from 'react'
+import { Group, LoadingManager } from 'three'
+import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import { saveJson, saveScene } from '../utils/modelFileTools'
 
 const manager = new LoadingManager()
 const loader = new GLTFLoader(manager)
@@ -16,6 +19,7 @@ function Viewer() {
   const { data, isLoading } = useDragFileUpload(viewerRef.current)
   const [message, setMessage] = useState('請拖曳上傳一個 gltf 檔案')
   const [isFirstLoad, setIsFirstLoad] = useState(false)
+  const [gltf, setGltf] = useState<GLTF>()
 
   useEffect(() => {
     if (isFirstLoad && data) {
@@ -37,6 +41,7 @@ function Viewer() {
       (gltf) => {
         sceneRef.current.add(gltf.scene)
         setIsFirstLoad(true)
+        setGltf(gltf)
       },
       undefined,
       (error) => {
@@ -46,12 +51,80 @@ function Viewer() {
     )
   }, [data, sceneRef, setMessage])
 
+  const handleGenerate = useCallback(() => {
+    if (!gltf) return
+    const tree = [] as any[]
+    const queue = [] as any[]
+    gltf.scene.children.forEach((child) => {
+      const node = {
+        id: child.name,
+        parent: null,
+        matrix4: arrayToString(child.matrix.toArray() as unknown as string[]),
+      }
+      //@ts-ignore
+      node.childs = child.children.map((child) => {
+        queue.push({ node: child, name: `${node.id}_${child.name}` })
+        return {
+          id: child.name,
+          //@ts-ignore
+          parent: node.id,
+          matrix4: null,
+          path: `${node.id}_${child.name}.glb`,
+        }
+      })
+      tree.push(node)
+      saveSocModel('SOC_Model', tree, queue)
+    })
+
+    async function saveSocModel(name: string, tree: any[], queue: any[]) {
+      while (queue.length) {
+        console.log(queue.length)
+        const { node, name } = queue.shift()
+        await saveSceneSync(node, name)
+      }
+      console.log({
+        version: '2.0.0',
+        name,
+        tree,
+      })
+      saveJson(
+        {
+          version: '2.0.0',
+          name,
+          tree,
+        },
+        'modelTiles',
+      )
+    }
+
+    function saveSceneSync(group: Group, name: string) {
+      return new Promise<void>((resolve) => {
+        saveScene(group, name)
+        setTimeout(() => {
+          resolve()
+        }, 1000)
+      })
+    }
+  }, [gltf])
+
   return (
     <>
       <div id="Message">{message}</div>
       <div id="Viewer" ref={viewerRef}></div>
+      <div id="UI">
+        <button onClick={handleGenerate}>生成漸進載入結構模型</button>
+      </div>
     </>
   )
 }
 
 export default Viewer
+
+//字串陣列轉字串逗號分隔
+function arrayToString(array: string[]) {
+  let str = ''
+  array.forEach((item) => {
+    str += item + ','
+  })
+  return str
+}
