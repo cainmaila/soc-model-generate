f18dff025e374a24d8edcca9e26c7184971a33f9
cain.chu <cainmaila>
2023-07-13 18:22:28 +0800

    檔案載入

--- src/view/Loader.tsx
+++ src/view/Loader.tsx
@@ -1,3 +1,21 @@
-export default function Loader() {
-  return <h1>Loader</h1>
+import { useLayoutEffect } from 'react'
+import { useThreeSceneInit } from '../hooks/threeSceneHooks'
+import { dtSocGroupLoader } from '../utils/dtSocGroupLoader'
+
+function Loader() {
+  const { viewerRef, sceneRef } = useThreeSceneInit()
+
+  useLayoutEffect(() => {
+    if (!viewerRef.current) return
+    const group = dtSocGroupLoader('/hq39', {
+      onProgress: (id: string) => {
+        console.log(id)
+      },
+    })
+    sceneRef.current.add(group)
+  }, [viewerRef, sceneRef])
+
+  return <div id="Viewer" ref={viewerRef}></div>
 }
+
+export default Loader
