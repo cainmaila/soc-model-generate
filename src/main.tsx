a44c2b068791c878a686dfd7a4898bd545404266
cain.chu <cainmaila>
2023-07-16 18:41:17 +0800

    css them

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { StrictMode } from 'react'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
