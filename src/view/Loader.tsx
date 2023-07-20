e57d179b7fbeaf6ab63d93893041369634e11193
cain.chu <cainmaila>
2023-07-20 23:09:01 +0800

    loading

--- src/view/Loader.tsx
+++ src/view/Loader.tsx
@@ -1,11 +1,14 @@
-import { useLayoutEffect } from 'react'
+import { useLayoutEffect, useState } from 'react'
 import { useThreeSceneInit } from '../hooks/threeSceneHooks'
 import { dtSocGroupLoader } from '../utils/dtSocGroupLoaderV3'
+import { LoadingBar } from '../components/LoadingBar'
 
 let isInit = false //是否初始化旗標，避掉重複初始化
 
 function Loader() {
   const { viewerRef, sceneRef } = useThreeSceneInit()
+  const [progress, setProgress] = useState(0)
+  const [loadingShow, setLoadingShow] = useState(false)
 
   useLayoutEffect(() => {
     if (!viewerRef.current) return
@@ -13,18 +16,25 @@ function Loader() {
     // const group = dtSocGroupLoader('hq39-v4/', {
     const group = dtSocGroupLoader('https://3d-models.eyeots.com/coral/0711/hq39-v4/', {
       root: '',
-      onProgress: (id: number) => {
-        console.log(id)
+      onProgress: (progress: number) => {
+        setLoadingShow(true)
+        setProgress(progress)
       },
       onComplete: () => {
         console.log('complete')
+        setLoadingShow(false)
       },
     })
     sceneRef.current.add(group)
     isInit = true
-  }, [viewerRef, sceneRef])
+  }, [viewerRef, sceneRef, setProgress])
 
-  return <div id="Viewer" ref={viewerRef}></div>
+  return (
+    <>
+      <LoadingBar progress={progress} show={loadingShow}></LoadingBar>
+      <div id="Viewer" ref={viewerRef}></div>
+    </>
+  )
 }
 
 export default Loader
