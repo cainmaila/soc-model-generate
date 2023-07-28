b41f2d94f3d5c16d23b67a97c7cad945eb4b66b3
cain.chu <cainmaila>
2023-07-28 17:45:56 +0800

    fly優化未整理

--- src/utils/threeTools.ts
+++ src/utils/threeTools.ts
@@ -33,6 +33,11 @@ export function setCameraToBestView(
   return box
 }
 
+let tween: gsap.core.Tween | null = null
+let tween2: gsap.core.Tween | null = null
+let tween3: gsap.core.Tween | null = null
+const sizeOb = { nowSize: 0 }
+
 /**
  * 飛到最佳視野
  * @param obj
@@ -43,19 +48,49 @@ export function setCameraToBestView(
 export function flyToBestView(obj: Object3D, camera: PerspectiveCamera, controls: OrbitControls) {
   const box = new Box3().setFromObject(obj)
   if (box.max.x === -Infinity) return null
-  const size = box.getSize(new Vector3()).length()
+  const size = box.getSize(new Vector3()).length() * 1.5 /* 距離 1~ */
+  const nowCenter = controls.target.clone()
   const center = box.getCenter(new Vector3())
   const po = center.clone()
+  /* 視角 */
   po.x += size / 2.0
-  po.y += size / 5.0
+  po.y += size / 10.0
   po.z += size / 2.0
-  controls.target.copy(center)
-  controls.maxDistance = size * 10
-  gsap.to(camera.position, {
+
+  if (tween) tween.kill()
+  if (tween2) tween2.kill()
+  if (tween3) tween3.kill()
+
+  tween3 = gsap.to(sizeOb, {
+    nowSize: size,
+    onUpdate: () => {
+      controls.maxDistance = sizeOb.nowSize * 10
+      // controls.update()
+    },
+    duration: 3,
+    onComplete: () => {
+      // controls.saveState()
+    },
+  })
+
+  tween2 = gsap.to(nowCenter, {
+    ...center,
+    onUpdate: () => {
+      camera.lookAt(nowCenter)
+      controls.target.copy(nowCenter)
+      // controls.update()
+    },
+    duration: 3,
+    onComplete: () => {
+      // controls.saveState()
+    },
+  })
+
+  tween = gsap.to(camera.position, {
     ...po,
     onUpdate: () => {
       camera.updateProjectionMatrix()
-      camera.lookAt(center)
+      // camera.lookAt(center)
       controls.update()
     },
     duration: 3,
